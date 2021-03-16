bb583fd76fcebdcb0067865b2ec02eb635f4e5a4
MarkoMilijevic <Marko.Milijevic@ocsbstudent.ca>
2021-03-16 22:34:05 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.getElementById('button').addEventListener('click', displayMessage)
 
 function displayMessage () {
-  alert("Hello")
+  alert('Hello')
 }
 
 document.getElementById('button2').addEventListener('click', changeFontStyle)
 
 function changeFontStyle () {
-  document.getElementById('paragraph').style.color="red"
-  document.getElementById('paragraph').style.fontSize="40px"
-  document.getElementById('paragraph').style.fontFamily="Comic Sans MS"
+  document.getElementById('paragraph').style.color = 'red'
+  document.getElementById('paragraph').style.fontSize = '40px'
+  document.getElementById('paragraph').style.fontFamily = 'Comic Sans MS'
 }
